119a3beb47941bd04b5e236fbd33f0283dbc5667
YatishPatil1234 <yatish.203869112@vcet.edu.in>
2023-10-17 00:08:39 +0530

    Initial commit

---
+++ fake_news_detection.py
@@ -0,0 +1,45 @@
+#Importing the libraries
+import pandas as pd
+import numpy as np
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pickle
+
+#Importing the cleaned file containing the text and label
+news = pd.read_csv('news.csv')
+X = news['text']
+y = news['label']
+
+#Splitting the data into train
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+#Creating a pipeline that first creates bag of words(after applying stopwords) & then applies Multinomial Naive Bayes model
+# pipeline = Pipeline([('tfidf', TfidfVectorizer(stop_words='english')),
+#                     ('nbmodel', MultinomialNB())])
+# pipeline = Pipeline([('tfidf', TfidfVectorizer(stop_words='english')),
+#                     ('rfmodel', RandomForestClassifier())])
+pipeline = Pipeline([('tfidf', TfidfVectorizer(stop_words='english')),
+                    ('svmmodel', SVC())])
+# pipeline = Pipeline([('tfidf', TfidfVectorizer(stop_words='english')),
+#                     ('lrmodel', LogisticRegression())])
+
+#Training our data
+pipeline.fit(X_train, y_train)
+
+#Predicting the label for the test data
+pred = pipeline.predict(X_test)
+
+#Checking the performance of our model
+print(classification_report(y_test, pred))
+print(confusion_matrix(y_test, pred))
+
+#Serialising the file
+with open('model.pickle', 'wb') as handle:
+    pickle.dump(pipeline, handle, protocol=pickle.HIGHEST_PROTOCOL)
